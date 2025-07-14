957983a2c498390961c8ec4cdbc9004983fa5fcc
Caleb Warren <slickwarren21@gmail.com>
2025-07-14 15:32:09 -0700

    add support for custom cluster provisioning

---
+++ tofu/rancher/custom_cluster/variables.tf
@@ -0,0 +1,36 @@
+variable "kubernetes_version" {}
+
+variable "is_network_policy" {
+    default = false
+    type    = bool
+}
+variable "psa" {
+    default = "" # "rancher-privileged"
+}
+variable "machine_pools" {
+  type = list(object({
+    control_plane_role        = optional(bool, false)
+    worker_role      = optional(bool, false)
+    etcd_role     = optional(bool, false)
+    quantity = optional(number, 1)
+  }))
+  default     = [{
+    control_plane_role = true
+    worker_role = true
+    etcd_role = true
+    quantity = 1
+  }]
+}
+
+variable "generate_name" {
+  type     = string
+  default = "ctw"
+  nullable = false
+}
+
+variable fqdn {}
+variable api_key {}
+variable insecure {
+    default = true
+    type    = bool
+}
