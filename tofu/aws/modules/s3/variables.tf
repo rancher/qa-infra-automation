98882d2ef38bc5f448458492b67168adf9b986d1
Leonardo Scalabrini <leonardo.scalabrini@suse.com>
2025-12-09 13:52:33 -0300

    review: Added variables types, description and default values

--- tofu/aws/modules/s3/variables.tf
+++ tofu/aws/modules/s3/variables.tf
@@ -1,5 +1,27 @@
-variable "bucket_name" {}
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
-variable "block_public_access" {}
+variable "bucket_name" {
+  type        = string
+  description = "S3 bucket name."
+}
+
+variable "aws_access_key" {
+  type        = string
+  sensitive   = true
+  description = "AWS access key."
+}
+
+variable "aws_secret_key" {
+  type        = string
+  sensitive   = true
+  description = "AWS secret key."
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region."
+}
+
+variable "block_public_access" {
+  type        = bool
+  default     = true
+  description = "Enable S3 block public access."
+}
