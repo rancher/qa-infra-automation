ca54f4e133257a5c4a3b4c4a7ea12c87e0c38871
Leonardo Scalabrini <leonardo.scalabrini@suse.com>
2026-08-24 15:27:02 -0300

    Import a cluster into rancher that was created on harvester infrastructure (#129)
    * feat: Added harvester rancher and dowsntream playbooks
    
    * feat: Update harvester vm to create the ssh key
    
    * feat: Add ssh key on .gitignore
    
    * review: auto review
    
    * review: updating README files
    
    ---------
    
    Co-authored-by: Leonardo Scalabrini <lscalabrini@suse.com>

--- tofu/harvester/modules/vm/outputs.tf
+++ tofu/harvester/modules/vm/outputs.tf
@@ -1,12 +1,22 @@
 output "ip" {
-  value = [for vm in harvester_virtualmachine.vm : vm.network_interface[0].ip_address] 
+  value = [for vm in harvester_virtualmachine.vm : vm.network_interface[0].ip_address]
 }
 
 output "kube_api_host" {
-  value = "${harvester_virtualmachine.vm[local.node_names[local.first_etcd_index].name].network_interface[0].ip_address}"
+  value       = harvester_virtualmachine.vm[local.node_names[local.first_etcd_index].name].network_interface[0].ip_address
   description = "The public IP address of the first etcd node, or 'No etcd node found'."
 }
 
 output "fqdn" {
   value = var.create_loadbalancer ? "${module.harvester_loadbalancer.ip_address}.sslip.io" : "${harvester_virtualmachine.vm[local.node_names[local.first_etcd_index].name].network_interface[0].ip_address}.sslip.io"
+}
+
+output "ssh_public_key" {
+  value       = local.ssh_public_key
+  description = "The public key used for the VMs, either provided via var.ssh_key or auto-generated."
+}
+
+output "ssh_private_key_path" {
+  value       = local.generate_ssh_key ? local.ssh_private_key_path : null
+  description = "Path to the auto-generated private key file, when one was generated."
 }
